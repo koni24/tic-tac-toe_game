9c7c145c7bfb730c557f10b280b058f16e60b6f5
koni24 <73066305+koni24@users.noreply.github.com>
2021-07-02 20:50:47 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -3,15 +3,25 @@ import GameView from"./GameView.js"
 let game=new Game();
 let gameView=new GameView();
 
+document.querySelector(".restart").addEventListener("click",()=>{
+    onRestartClick();
+})
 
-let tiles=document.querySelector(".board-tile")
+let tiles=document.querySelectorAll(".board-tile");
 tiles.forEach((tile)=>{
    tile.addEventListener("click",()=>{
-       console.log(tile.dataset.index);
+       onTileClick(tile.dataset.index);
    })
 })
 
 
 function onTileClick(i){
-
+    game.makeMove(i);
+    gameView.updateBoard(game);
 }
+function onRestartClick(){
+    game=new Game();
+    gameView.updateBoard(game);
+}
+
+gameView.updateBoard(game);
